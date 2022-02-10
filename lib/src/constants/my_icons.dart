d9c3d31a3a88156cc1dd273c3190c82c86e00366
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-02-10 09:39:57 +0700

    Declaring resolution-aware image assets

--- lib/src/constants/my_icons.dart
+++ lib/src/constants/my_icons.dart
@@ -1,13 +1,14 @@
 class MyIcons {
-  static const String homeIcon = "assets/icons/home.png";
-  static const String shopIcon = "assets/icons/shop.png";
-  static const String bagIcon = "assets/icons/bag.png";
-  static const String favoriteIcon = "assets/icons/favorite.png";
-  static const String profileIcon = "assets/icons/profile.png";
+  static const String homeIcon = "assets/images/ic_home.png";
+  static const String shopIcon = "assets/images/ic_shop.png";
+  static const String bagIcon = "assets/images/ic_bag.png";
+  static const String favoriteIcon = "assets/images/ic_favorite.png";
+  static const String profileIcon = "assets/images/ic_profile.png";
 
-  static const String activeHomeIcon = "assets/icons/active_home.png";
-  static const String activeShopIcon = "assets/icons/active_shop.png";
-  static const String activeBagIcon = "assets/icons/active_bag.png";
-  static const String activeFavoriteIcon = "assets/icons/active_favorite.png";
-  static const String activeProfileIcon = "assets/icons/active_profile.png";
+  static const String activeHomeIcon = "assets/images/ic_active_home.png";
+  static const String activeShopIcon = "assets/images/ic_active_shop.png";
+  static const String activeBagIcon = "assets/images/ic_active_bag.png";
+  static const String activeFavoriteIcon =
+      "assets/images/ic_active_favorite.png";
+  static const String activeProfileIcon = "assets/images/ic_active_profile.png";
 }
