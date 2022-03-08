4234981f5cf108379ccb08d4c863705379a65514
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-03-08 18:25:11 +0700

    clear problems

--- lib/src/modules/product_by_category/logic/product_by_category_state.dart
+++ lib/src/modules/product_by_category/logic/product_by_category_state.dart
@@ -48,33 +48,6 @@ extension ViewTypeExt on ViewType {
   }
 }
 
-enum ViewType { listView, gridView }
-
-extension ViewTypeExt on ViewType {
-  String iconOf() {
-    switch (this) {
-      case ViewType.listView:
-        return MyIcons.listIcon;
-      case ViewType.gridView:
-        return MyIcons.gridIcon;
-
-      default:
-        return "N/A";
-    }
-  }
-
-  Color backgroundColor() {
-    switch (this) {
-      case ViewType.gridView:
-        return MyColors.colorWhite;
-      case ViewType.listView:
-        return MyColors.colorBackground2;
-      default:
-        return MyColors.colorPrimary;
-    }
-  }
-}
-
 enum SortBy {
   popular,
   newest,
