7ac3c90c804b20a58e3a82c08984ca505e40c1aa
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-03-21 11:04:57 +0700

    remove background payment card

--- lib/src/modules/payment_method/widgets/payment_card_master.dart
+++ lib/src/modules/payment_method/widgets/payment_card_master.dart
@@ -1,6 +1,5 @@
 import 'package:e_commerce/src/config/themes/my_colors.dart';
 import 'package:e_commerce/src/constants/my_images.dart';
-import 'package:e_commerce/src/modules/payment_method/widgets/custom_background_card.dart';
 import 'package:flutter/material.dart';
 
 class PaymentCardMaster extends StatelessWidget {
@@ -21,68 +20,63 @@ class PaymentCardMaster extends StatelessWidget {
                 spreadRadius: 1)
           ],
           borderRadius: const BorderRadius.all(Radius.circular(8))),
-      child: Stack(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const CustomBackgroundCard(),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Image.asset(
+            MyImages.chipCardPayment,
+            fit: BoxFit.cover,
+          ),
+          const Text(
+            '* * * *  * * * *  * * * *  3947',
+            style: TextStyle(
+                fontSize: 22,
+                color: MyColors.colorWhite,
+                fontWeight: FontWeight.bold,
+                height: 1),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              RichText(
+                text: const TextSpan(
+                  text: 'Card Holder Name\n',
+                  style: TextStyle(
+                      fontSize: 10,
+                      height: 1.42,
+                      fontWeight: FontWeight.w600,
+                      color: MyColors.colorWhite),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Jennyfer Doe',
+                        style: TextStyle(
+                          fontSize: 14,
+                        )),
+                  ],
+                ),
+              ),
+              RichText(
+                text: const TextSpan(
+                  text: 'Expiry Date\n',
+                  style: TextStyle(
+                      fontSize: 10,
+                      height: 1.42,
+                      fontWeight: FontWeight.w600,
+                      color: MyColors.colorWhite),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: '05/23',
+                        style: TextStyle(
+                          fontSize: 14,
+                        )),
+                  ],
+                ),
+              ),
               Image.asset(
-                MyImages.chipCardPayment,
+                MyImages.masterCardPayment2,
                 fit: BoxFit.cover,
-              ),
-              const Text(
-                '* * * *  * * * *  * * * *  3947',
-                style: TextStyle(
-                    fontSize: 22,
-                    color: MyColors.colorWhite,
-                    fontWeight: FontWeight.bold,
-                    height: 1),
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  RichText(
-                    text: const TextSpan(
-                      text: 'Card Holder Name\n',
-                      style: TextStyle(
-                          fontSize: 10,
-                          height: 1.42,
-                          fontWeight: FontWeight.w600,
-                          color: MyColors.colorWhite),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: 'Jennyfer Doe',
-                            style: TextStyle(
-                              fontSize: 14,
-                            )),
-                      ],
-                    ),
-                  ),
-                  RichText(
-                    text: const TextSpan(
-                      text: 'Expiry Date\n',
-                      style: TextStyle(
-                          fontSize: 10,
-                          height: 1.42,
-                          fontWeight: FontWeight.w600,
-                          color: MyColors.colorWhite),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: '05/23',
-                            style: TextStyle(
-                              fontSize: 14,
-                            )),
-                      ],
-                    ),
-                  ),
-                  Image.asset(
-                    MyImages.masterCardPayment2,
-                    fit: BoxFit.cover,
-                  )
-                ],
               )
             ],
           )
--- lib/src/modules/payment_method/widgets/payment_card_visa.dart
+++ lib/src/modules/payment_method/widgets/payment_card_visa.dart
@@ -1,6 +1,5 @@
 import 'package:e_commerce/src/config/themes/my_colors.dart';
 import 'package:e_commerce/src/constants/my_images.dart';
-import 'package:e_commerce/src/modules/payment_method/widgets/custom_background_card.dart';
 import 'package:flutter/material.dart';
 
 class PaymentCardVisa extends StatelessWidget {
@@ -21,71 +20,67 @@ class PaymentCardVisa extends StatelessWidget {
                 spreadRadius: 1)
           ],
           borderRadius: const BorderRadius.all(Radius.circular(8))),
-      child: Stack(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const CustomBackgroundCard(),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Align(
+            alignment: Alignment.centerRight,
+            child: Image.asset(
+              MyImages.visaCardPayment,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const Text(
+            '* * * *  * * * *  * * * *  4546',
+            style: TextStyle(
+                fontSize: 22,
+                color: MyColors.colorWhite,
+                fontWeight: FontWeight.bold,
+                height: 1),
+          ),
+          Image.asset(
+            MyImages.chipCardPayment,
+            fit: BoxFit.cover,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Align(alignment: Alignment.centerRight,
-                child: Image.asset(
-                  MyImages.visaCardPayment,
-                  fit: BoxFit.cover,
+              RichText(
+                text: const TextSpan(
+                  text: 'Card Holder Name\n',
+                  style: TextStyle(
+                      fontSize: 10,
+                      height: 1.42,
+                      fontWeight: FontWeight.w600,
+                      color: MyColors.colorWhite),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Jennyfer Doe',
+                        style: TextStyle(
+                          fontSize: 14,
+                        )),
+                  ],
                 ),
               ),
-              const Text(
-                '* * * *  * * * *  * * * *  4546',
-                style: TextStyle(
-                    fontSize: 22,
-                    color: MyColors.colorWhite,
-                    fontWeight: FontWeight.bold,
-                    height: 1),
+              RichText(
+                text: const TextSpan(
+                  text: 'Expiry Date\n',
+                  style: TextStyle(
+                      fontSize: 10,
+                      height: 1.42,
+                      fontWeight: FontWeight.w600,
+                      color: MyColors.colorWhite),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: '11/22',
+                        style: TextStyle(
+                          fontSize: 14,
+                        )),
+                  ],
+                ),
               ),
-              Image.asset(
-                MyImages.chipCardPayment,
-                fit: BoxFit.cover,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  RichText(
-                    text: const TextSpan(
-                      text: 'Card Holder Name\n',
-                      style: TextStyle(
-                          fontSize: 10,
-                          height: 1.42,
-                          fontWeight: FontWeight.w600,
-                          color: MyColors.colorWhite),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: 'Jennyfer Doe',
-                            style: TextStyle(
-                              fontSize: 14,
-                            )),
-                      ],
-                    ),
-                  ),
-                  RichText(
-                    text: const TextSpan(
-                      text: 'Expiry Date\n',
-                      style: TextStyle(
-                          fontSize: 10,
-                          height: 1.42,
-                          fontWeight: FontWeight.w600,
-                          color: MyColors.colorWhite),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: '11/22',
-                            style: TextStyle(
-                              fontSize: 14,
-                            )),
-                      ],
-                    ),
-                  ),
-                ],
-              )
             ],
           )
         ],
