31c852ccc8290ec7468b772e0e755298d75b1655
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-04-06 02:20:18 +0700

    build UI success screen two option

--- lib/src/constants/my_images.dart
+++ lib/src/constants/my_images.dart
@@ -7,4 +7,6 @@ class MyImages {
   static const fedexDelivery = "assets/images/fedex.png";
   static const visaCardPayment = "assets/images/visa.png";
   static const chipCardPayment = "assets/images/chip.png";
+  static const bannerSuccess = "assets/images/banner_success.png";
+  static const bannerSuccess2 = "assets/images/banner_success2.png";
 }
