4a50cc23e5d785d4fb5bf2f0db8fe4a2c6fca106
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-04-15 11:49:22 +0700

    Implement navigtor to payment, address, logout

--- lib/src/modules/profile/widgets/list_tile_profile.dart
+++ lib/src/modules/profile/widgets/list_tile_profile.dart
@@ -1,6 +1,8 @@
 import 'package:e_commerce/src/config/themes/my_colors.dart';
 import 'package:e_commerce/src/modules/account/logic/account_bloc.dart';
+import 'package:e_commerce/src/modules/dashboard/router/dashboard_router.dart';
 import 'package:e_commerce/src/modules/profile/router/profile_router.dart';
+import 'package:e_commerce/src/utils/enum/extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -21,57 +23,76 @@ class ListTileProfile extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     final padding = size.width * 0.037;
     final paddingContent = EdgeInsets.fromLTRB(padding, 0, padding, 0);
-    final List<XFunctionProfile> _items = [
-      XFunctionProfile(
-          subtitle: 'Already have 12 orders',
-          title: 'My orders',
-          onTap: () {
-            context.read<AccountBloc>().logout(context);
-          }),
-      XFunctionProfile(
-          subtitle: '3 addresses', title: 'Shipping addresses', onTap: () {}),
-      XFunctionProfile(
-          subtitle: 'Visa **34', title: 'Payment methods', onTap: () {}),
-      XFunctionProfile(
-          subtitle: 'You have special promocodes',
-          title: 'Promocodes',
-          onTap: () {}),
-      XFunctionProfile(
-          subtitle: 'Reviews for 4 items',
-          title: 'My reviews',
-          onTap: () => ProfileCoordinator.showNotification(context)),
-      XFunctionProfile(
-          subtitle: 'Infomation, password',
-          title: 'Settings',
-          onTap: () => ProfileCoordinator.showSetting(context)),
-      XFunctionProfile(
-          subtitle: 'Notifications',
-          title: 'Notification',
-          onTap: () => ProfileCoordinator.showNotification(context)),
-    ];
-    return Column(
-        children: ListTile.divideTiles(
-            color: MyColors.colorGray,
-            tiles: _items.map((item) => ListTile(
-                  dense: true,
-                  contentPadding: paddingContent,
-                  onTap: () => item.onTap!(),
-                  title: Text(item.title,
-                      style: const TextStyle(
-                          fontSize: 16,
-                          height: 1,
-                          color: MyColors.colorBlack,
-                          fontWeight: FontWeight.w600)),
-                  subtitle: Text(item.subtitle,
-                      style: const TextStyle(
-                          fontSize: 11,
-                          height: 1,
-                          color: MyColors.colorGray,
-                          fontWeight: FontWeight.normal)),
-                  trailing: const Icon(
-                    Icons.arrow_forward_ios_outlined,
-                    color: MyColors.colorGray,
-                  ),
-                ))).toList());
+
+    return BlocBuilder<AccountBloc, AccountState>(
+      builder: (context, state) {
+        final String subtitlePayment;
+        if (state.paymentMethodDefault.id != 'N/A') {
+          subtitlePayment =
+              '${state.paymentMethodDefault.type == 1 ? "Mater Card" : 'Visa'} **${state.paymentMethodDefault.cardNumber.toString().lastChars(2)}';
+        } else {
+          subtitlePayment = 'No card';
+        }
+        final List<XFunctionProfile> _items = [
+          XFunctionProfile(
+              subtitle: 'Already have 12 orders',
+              title: 'My orders',
+              onTap: () {}),
+          XFunctionProfile(
+              subtitle:
+                  '${(state.data.shippingAddresses ?? []).length} addresses',
+              title: 'Shipping addresses',
+              onTap: () => DashboardCoordinator.showShippingAddresses(context)),
+          XFunctionProfile(
+              subtitle: subtitlePayment,
+              title: 'Payment methods',
+              onTap: () => DashboardCoordinator.showPaymentMethod(context)),
+          XFunctionProfile(
+              subtitle: 'You have special promocodes',
+              title: 'Promocodes',
+              onTap: () {}),
+          XFunctionProfile(
+              subtitle: 'Reviews for 4 items',
+              title: 'My reviews',
+              onTap: () => ProfileCoordinator.showNotification(context)),
+          XFunctionProfile(
+              subtitle: 'Infomation, password',
+              title: 'Settings',
+              onTap: () => ProfileCoordinator.showSetting(context)),
+          XFunctionProfile(
+              subtitle: 'List Notifications',
+              title: 'Notification',
+              onTap: () => ProfileCoordinator.showNotification(context)),
+          XFunctionProfile(
+              subtitle: 'Log out account',
+              title: 'Logout',
+              onTap: () => context.read<AccountBloc>().logout(context)),
+        ];
+        return Column(
+            children: ListTile.divideTiles(
+                color: MyColors.colorGray,
+                tiles: _items.map((item) => ListTile(
+                      dense: true,
+                      contentPadding: paddingContent,
+                      onTap: () => item.onTap!(),
+                      title: Text(item.title,
+                          style: const TextStyle(
+                              fontSize: 16,
+                              height: 1,
+                              color: MyColors.colorBlack,
+                              fontWeight: FontWeight.w600)),
+                      subtitle: Text(item.subtitle,
+                          style: const TextStyle(
+                              fontSize: 11,
+                              height: 1,
+                              color: MyColors.colorGray,
+                              fontWeight: FontWeight.normal)),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: MyColors.colorGray,
+                      ),
+                    ))).toList());
+      },
+    );
   }
 }
