bbeea167fac707c54f10489afc684445b0838408
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-04-20 15:38:33 +0700

    login google website

--- lib/src/repositories/firestore/services/auth_service.dart
+++ lib/src/repositories/firestore/services/auth_service.dart
@@ -11,6 +11,8 @@ class AuthService {
 
   Future<UserCredential> userCredential(AuthCredential credential) async =>
       await auth.signInWithCredential(credential);
+  Future<UserCredential> userCredentialWeb(GoogleAuthProvider provider) async =>
+      await auth.signInWithPopup(provider);
 
   Future<UserCredential> signInWithEmailAndPassword(
           {String email = "", String password = ""}) async =>
--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,5 +1,9 @@
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-analytics.js");
+
+
 
 firebase.initializeApp({
   apiKey: "AIzaSyBnMkIiziYTEqM6a5qaoRZXZScTGo30958",
